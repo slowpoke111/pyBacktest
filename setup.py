9ecc9404d85c12bfd6c3cb3aea68544ab230767d
Ben Bell <110692534+slowpoke111@users.noreply.github.com>
2024-11-08 22:04:13 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup, find_packages
-
 setup(
     name="pyBacktest",
     version="0.0.1",
     author="Ben Bell",
     author_email="slow111poke@gmail.com",
     description="A backtesting framework for stock price prediction strategies",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/slowpoke111/pyBacktest/",
     packages=find_packages(include=['pyBacktest', 'pyBacktest.*']),
     install_requires=[
         "pandas",
@@ -16,5 +17,5 @@ setup(
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.9',
 )
